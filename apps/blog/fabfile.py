871fff2c69e98f6bc00dc5cb4f038611d7290e24
Anders Horsted <anders@alphabits.dk>
2011-05-15 00:56:37 +0200

    Deployment commit

--- apps/blog/fabfile.py
+++ apps/blog/fabfile.py
@@ -22,6 +22,7 @@ def deploy():
     code_dir = '/home/anders/alphabits.dk/application'
     local('git add .')
     local('git commit -m "Deployment commit"')
+    local('git push origin master')
     with cd(code_dir):
         local('git pull origin master')
         local('touch app.wsgi')
