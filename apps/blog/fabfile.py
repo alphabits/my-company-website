86566456f671443d45935c19e1007dcbbc7273ad
Anders Horsted <anders@alphabits.dk>
2011-05-15 00:53:55 +0200

    Deployment commit

--- apps/blog/fabfile.py
+++ apps/blog/fabfile.py
@@ -2,9 +2,11 @@ from __future__ import with_statement
 
 import os
 
-from fabric.api import local, run
+from fabric.api import *
 import yaml
 
+
+env.hosts = ['anders@173.45.237.53:30010']
 
 def test():
     index_data = {}
@@ -15,6 +17,14 @@ def test():
         index_data[post['date']] = filename
         output = yaml.dump(index_data, open('posts/_index_file.yaml', 'w'), indent=4,
                 default_flow_style=False)
+
+def deploy():
+    code_dir = '/home/anders/alphabits.dk/application'
+    local('git add .')
+    local('git commit -m "Deployment commit"')
+    with cd(code_dir):
+        local('git pull')
+        local('touch app.wsgi')
 
 def ext_test():
     run('ls')
